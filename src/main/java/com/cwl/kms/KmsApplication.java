82ab7a71f0ae8cd0d524108264e7f765a9c2f235
chenwenlong-java <18028666849@163.com>
2023-09-15 21:00:52 +0800

    feature: init project

---
+++ src/main/java/com/cwl/kms/KmsApplication.java
@@ -0,0 +1,13 @@
+package com.cwl.kms;
+
+/**
+ * ClassName: KmsApplication
+ * Package: com.cwl.kms
+ * Description:
+ *
+ * @Author chenwenlong
+ * @Create 2023/9/15 20:58
+ * @Version 1.0
+ */
+public class KmsApplication {
+}
